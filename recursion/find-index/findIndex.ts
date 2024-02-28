f73f03b045e5a9a8e1e48ccd3f93889eac161ba9
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-02-27 16:11:54 -0800

    finished gather strings, refactored findIndex

--- recursion/find-index/findIndex.ts
+++ recursion/find-index/findIndex.ts
@@ -1,26 +1,22 @@
 /** findIndex: return index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr: number[], val: number): number {
-  function findIndex(arr: number[], val: number): number {
+function findIndex(arr: number[], val: number, idx: number = 0): number {
 
-    function _valCheck(arr: number[], i: number):number {
-      if (arr.length === i) return -1;
-      if (arr[i] === val) return i;
+  if (arr.length === idx) return -1;
+  if (arr[idx] === val) return idx;
 
-      return _valCheck(arr, i + 1);
-    }
-
-    return _valCheck(arr, 0);
-  }
+  return findIndex(arr, val, idx + 1);
 }
+
 
 export { findIndex };
 
-  // if (arr.length === 0) return -1;
-  // if (arr[0] === val) return 0;
+// if (arr.length === 0) return -1;
+// if (arr[0] === val) return 0;
 
-  // const indexOnRest = findIndex(arr.slice(1), val)
+// const indexOnRest = findIndex(arr.slice(1), val)
 
-  // if
+// if
 
-  // return (indexOnRest === -1) ? -1 : 1 + indexOnRest
+// return (indexOnRest === -1) ? -1 : 1 + indexOnRest
+
--- recursion/gather-strings/gatherStrings.ts
+++ recursion/gather-strings/gatherStrings.ts
@@ -1,21 +1,18 @@
 /** gatherStrings: given an object, return array of all the string values. */
 
 function gatherStrings(obj: Record<string, any>): string[] {
-  // base case where no keys left
-  // loop through obj
-  // if value is string
-  // return [string + ...gatherString(obj - that key)]
+  let output: string[] = []
 
-  if (obj.keys.length === 0) return [];
-
-  const keyToEvaluate = obj.keys[0]
-  const valueToEvaluate = obj[keyToEvaluate]
-  if (typeof valueToEvaluate === "string"){
-    delete obj.keyToEvaluate
-    return [valueToEvaluate, ...gatherStrings(obj)]
+  for (const key in obj) {
+    if (typeof obj[key] === "string") {
+      output.push(obj[key]);
+    }
+    else if (typeof obj[key] === "object") {
+      output = output.concat(gatherStrings(obj[key]))
+    }
   }
 
-  return gatherStrings(obj)
+  return output;
 }
 
 export { gatherStrings };
